9d06c0669448782efcf7841381696045bf71faf5
Maximilian Janisch <maximilian@janisch.me>
2020-01-07 12:24:21 +0100

    Completely restructured helpers file: Much better performance now
    Small improvements to main.py

--- RandomForestImplementation/main.py
+++ RandomForestImplementation/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from RandomForestImplementation.helpers import find_best_split_of_all
+from RandomForestImplementation.helpers import best_split
 
 
 class Tree:
@@ -20,26 +20,32 @@ class DecisionTreeClassifier(object):
         self.max_depth = max_depth
 
     def fit(self, x, y):
+        n_classes = len(np.unique(y))
+        n_features = x.shape[1]
         depth = 0
         base = Tree(feature_name="base", prediction=np.round(np.mean(y)))
         queue = [(base, x, y, depth)]
 
-        while 0 < len(queue):
-            # print(len(queue))
+        while queue:
             """
             Each node in the queue which are just terminal nodes get replaced by a split->prediction1/prediction2 construct.
             The old node gets removed from the queue and the new nodes get added to it
             """
             c, x, y, d = queue.pop(0)
-            if len(y) == 0 or d + 1 >= self.max_depth or len(np.unique(y)) == 1: continue
-            # Adding a new layer would exceed the maximum depth of the tree or we have no more data left
-            # or further fitting to our data would not change the predictions (if all the y are the same)
+            if len(np.unique(y)) <= 1 or d + 1 >= self.max_depth or len(np.unique(y)) == 1:
+                """
+                Adding a new layer would exceed the maximum depth of the tree or we have no more data left
+                or further fitting to our data would not change the predictions (this happens if all the y are the same)
+                """
+                continue
 
-            col, cutoff, entropy = find_best_split_of_all(x, y)  # find the best split for the data at the node
+            # print("Trying to get split") TODO: remove
+            col, cutoff = best_split(x, y, n_classes, n_features)  # find the best split for the data at the node
+            # print("Got split") TODO: remove
             filter_ = x[:, col] < cutoff
             x_left, y_left = x[filter_], y[filter_]
             x_right, y_right = x[np.logical_not(filter_)], y[np.logical_not(filter_)]
-            # print(x_left, y_left, x_right, y_right, sep="\n")
+            # print(x_left, y_left, x_right, y_right, sep="\n") TODO: remove
 
             left_c, right_c = Tree(prediction=np.round(np.mean(y_left))), Tree(prediction=np.round(np.mean(y_right)))
             c.left, c.right = left_c, right_c
@@ -61,10 +67,10 @@ class DecisionTreeClassifier(object):
 
 
 if __name__ == '__main__':  # Test
-    from sklearn.datasets import load_iris
+    from sklearn.datasets import load_digits
 
-    iris = load_iris()
-    x = iris.data
-    y = iris.target
-    a = DecisionTreeClassifier(7)
-    b = a.fit(x, y)
+    digits = load_digits()
+    x = digits.data
+    y = digits.target
+    a = DecisionTreeClassifier(5)
+    classifier = a.fit(x, y)
